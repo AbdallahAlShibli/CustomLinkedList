1a5a64a0104c38785cf571c543fa9568c55c0c8e
Abdullah <48906718+AbdallahAlShibli@users.noreply.github.com>
2023-06-07 22:21:07 +0400

    Add project files.

---
+++ CustomLinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CustomLinkedList;
+namespace LinkedList
+{
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CustomLinkedList.LinkedList<string> ll = new CustomLinkedList.LinkedList<string>();
+
+            Node<string> a = new Node<string>("AAA");
+            ll.AddFirst(a);
+            Node<string> b = new Node<string>("BBB");
+            ll.AddFirst(b);
+            Node<string> c = new Node<string>("CCC");
+            ll.AddFirst(c);
+            Node<string> d = new Node<string>("DDD");
+            ll.AddFirst(d);
+            ll.Traverse();
+            ll.AddAfter(new Node<string>("xxx"), a);
+
+            ll.Traverse();
+
+            Console.ReadKey();
+        }
+    }
+
+}
